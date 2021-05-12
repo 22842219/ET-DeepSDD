03395cab41443dace6a5b7ee86521b523a593f98
Ziyu Zhao <ziyu@agent.csse.uwa.edu.au.ubuntu>
2021-05-12 20:54:40 +0800

    update

--- build_data.py
+++ build_data.py
@@ -30,8 +30,7 @@ class Sentence(object):
 				labels,  
 				wordpieces, 				 
 				wordpiece_ids, 
-				token_idxs_to_wp_idxs,  
-				build_labels = True):
+				token_idxs_to_wp_idxs):
 	
 		self.tokens = tokens
 		self.token_ids = token_ids
@@ -42,51 +41,17 @@ class Sentence(object):
 
 		self.token_idxs_to_wp_idxs = token_idxs_to_wp_idxs
 
-		# The build_labels variable is set to False when constructing sentences from the testing set (I think!)
-		if build_labels:
-			self.mentions = self.get_mentions_vector(self.labels)			
-			self.wordpiece_labels = self.get_wordpiece_labels(self.wordpieces, self.labels, self.token_idxs_to_wp_idxs)			
-			self.wordpiece_mentions = self.get_mentions_vector(self.wordpiece_labels)			
-			self.pad_labels()
-			self.pad_wordpiece_labels()
-
-	def pad_wordpiece_labels(self):
-		for x in range(len(self.wordpiece_labels)):
-			self.wordpiece_labels.append([0] * len(self.wordpiece_labels[0]))
-
-
-	def pad_labels(self):
-		for x in range(len(self.labels)):
-			self.labels.append([0] * len(self.labels[0]))
-
-	def get_wordpiece_labels(self, wordpieces, labels, token_idxs_to_wp_idxs):
-		wordpiece_labels = []
-		padding_labels = [0] * len(labels[0])
-		for i, idx in enumerate(token_idxs_to_wp_idxs):
-			if i == len(token_idxs_to_wp_idxs) - 1:
-				max_idx = len(wordpieces)
-			else:
-				max_idx = token_idxs_to_wp_idxs[i + 1]
-			for x in range(idx, max_idx):
-				wordpiece_labels.append(labels[i])
-		return [padding_labels] + wordpiece_labels + [padding_labels] # Add 'padding_labels' for the [CLS] and [SEP] wordpieces
-
-	# Retrieve the mentions vector, a list of 0s and 1s, where 1s represent that the token at that index is an entity.
-	def get_mentions_vector(self, labels):
-		return [1 if 1 in x else 0 for x in labels]
-
 	# Return the data corresponding to this sentence.
 	def data(self):
-		return self.tokens, self.labels, self.mentions, self.wordpieces, self.wordpiece_labels, self.wordpiece_mentions, self.token_idxs_to_wp_idxs
+		return self.tokens, self.token_ids, self.labels, self.wordpieces, self.wordpiece_ids, self.token_idxs_to_wp_idxs
 
 	# Print out a summary of the sentence.
 	def __repr__(self):
-		s =  "Tokens:             %s\n" % self.tokens
-		s += "Labels:             %s\n" % self.labels 
-		s += "Mentions:           %s\n" % self.mentions 
-		s += "Wordpieces:         %s\n" % self.wordpieces 
-		s += "Wordpiece labels:   %s\n" % self.wordpiece_labels 
-		s += "Wordpiece mentions: %s\n" % self.wordpiece_mentions
+		s =  "tokens:             %s\n" % self.tokens
+		s += "token_ids:          %s\n" % self.token_ids 
+		s += "labels:           %s\n" % self.labels 
+		s += "wordpieces:         %s\n" % self.wordpieces 
+		s += "wordpiece_ids:   %s\n" % self.wordpiece_ids 
 		s += "Token map:          %s\n" % self.token_idxs_to_wp_idxs
 		return s
 
@@ -102,7 +67,7 @@ class Mention(Sentence):
 				token_idxs_to_wp_idxs, 
 				start, 
 				end):
-		super(Mention, self).__init__(tokens, token_ids, labels, wordpieces, wordpiece_ids, token_idxs_to_wp_idxs, build_labels = False)		
+		super(Mention, self).__init__(tokens, token_ids, labels, wordpieces, wordpiece_ids, token_idxs_to_wp_idxs)		
 
 
 		self.token_mention_start = start
@@ -118,6 +83,7 @@ class Mention(Sentence):
 		
 	def build_context_wordpieces(self):
 		token_span 	 = cf.MODEL_OPTIONS['context_window']
+		mention_span = cf.MODEL_OPTIONS['mention_window']
 		wp_l_span = max(0, self.token_mention_start - token_span)
 		wp_r_span  = min(self.token_mention_end + token_span, len(self.token_ids)) 
 	
@@ -133,11 +99,13 @@ class Mention(Sentence):
 		self.wp_ids_mention = self.wordpiece_ids[self.wp_mention_start : self.wp_mention_end]	
 		self.wp_ids_all = self.wp_ids_left + self.wp_ids_mention + self.wp_ids_right
 
+		self.wp_ids_left_padding = self.wp_ids_left + [0]*(token_span - len(self.wp_ids_left ))
+		self.wp_ids_right_padding = self.wp_ids_right + [0]*(token_span - len(self.wp_ids_right ))
+		self.wp_ids_mention_padding = self.wp_ids_left + [0]*(mention_span - len(self.wp_ids_mention ))
+		self.wp_ids_all_padding =  self.wp_ids_left_padding + self.wp_ids_mention_padding + self.wp_ids_right_padding
 
 
 
-
-	
 
 	def is_valid(self):
 		maxlen 	 = cf.MODEL_OPTIONS['context_window']
@@ -194,12 +162,6 @@ def build_dataset(filepath, hierarchy, ds_name, contextualizer, tokenizer):
 			tokens = line['tokens']
 			total_sents += 1
 
-			sent_train_encodings = tokenizer.encode_plus(tokens, 
-									is_split_into_words=True, 
-									return_offsets_mapping=True, 
-									padding=True, 
-									truncation=True)
-
 			wordpiece_ids_without_special_symbols, wordpiece_ids, token_idxs_to_wp_idxs = contextualizer.tokenize_with_mapping(tokens)	
 			wordpieces = tokenizer.convert_ids_to_tokens(wordpiece_ids)
 			token_ids = tokenizer.convert_tokens_to_ids(tokens)	
@@ -241,13 +203,13 @@ def build_dataset(filepath, hierarchy, ds_name, contextualizer, tokenizer):
 	data_xl, data_xr, data_xa, data_xm, data_y = [], [], [], [], []
 
 	for i, mention in enumerate(mentions):
-		data_xl.append(np.asarray(mention.wp_ids_left))
-		data_xr.append(np.asarray(mention.wp_ids_right))
-		data_xa.append(np.asarray(mention.wp_ids_all))
-		data_xm.append(np.asarray(mention.wp_ids_mention))
+		data_xl.append(np.asarray(mention.wp_ids_left_padding))
+		data_xr.append(np.asarray(mention.wp_ids_right_padding))
+		data_xa.append(np.asarray(mention.wp_ids_all_padding))
+		data_xm.append(np.asarray(mention.wp_ids_mention_padding))
 		data_y.append(np.asarray(mention.labels))
 	
-	
+	print("data_xl:", len(data_xl))
 	dataset = MentionTypingDataset(data_xl, data_xr, data_xa, data_xm, data_y)
 
 	return dataset, total_wordpieces
